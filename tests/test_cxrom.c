842a5f2deb4bbcc8e73bf564ed76e0a215d2646a
Samir Sinha <samir@cinekine.com>
2021-03-18 08:18:20 -0700

    Added more test source files (unimplemented)

---
+++ tests/test_cxrom.c
@@ -0,0 +1,8 @@
+/*
+    Tests $Cxxx ROM options (not specific peripherals, just the switches.)
+
+    -   INTCXROM
+    -   INTC3ROM
+    -   SLOTCXROM
+    -   SLOTC3ROM
+*/
---
+++ tests/test_shadow_00_01.c
@@ -0,0 +1,8 @@
+/*
+    Tests shadowed writes from 00,01 to E0,EI
+
+    Cases:
+        - Shadow register options (not including shadow-all-banks)
+        - Write vs Read timings
+        - Shadowed pages (text, graphics, exclude iolc)
+*/
