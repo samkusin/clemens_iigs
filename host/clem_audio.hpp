88d23ca641026b6e11e3bb06b69cb96c2c321d45
Samir Sinha <samir@cinekine.com>
2022-10-28 15:24:28 -0700

    Testing switch to sokol_audio

--- host/clem_audio.hpp
+++ host/clem_audio.hpp
@@ -1,10 +1,10 @@
 #ifndef CLEM_HOST_AUDIO_H
 #define CLEM_HOST_AUDIO_H
 
-#include "ckaudio/core.h"
 #include "clem_types.h"
 
 #include <vector>
+#include <mutex>
 
 
 class ClemensAudioDevice {
@@ -20,18 +20,17 @@ public:
   unsigned queue(ClemensAudio &audio, float deltaTime);
 
 private:
-  static uint32_t mixAudio(CKAudioBuffer *audioBuffer, CKAudioTimePoint *timepoint, void *ctx);
+  static void mixAudio(float* buffer, int num_frames, int num_channels, void* user_data);
 
-  uint32_t mixClemensAudio(CKAudioBuffer *outBuffer, uint32_t outFramesAvailable);
-
-  CKAudioDataFormat dataFormat_;
-  CKAudioTimePoint lastTimepoint_;
+  void mixClemensAudio(float* buffer, int num_frames, int num_channels);
 
   uint8_t* queuedFrameBuffer_;
   uint32_t queuedFrameHead_;
   uint32_t queuedFrameTail_;
   uint32_t queuedFrameLimit_;
   uint32_t queuedFrameStride_;
+
+  std::mutex queuedFrameMutex_;
 };
 
 #endif
