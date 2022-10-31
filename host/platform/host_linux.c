fcb35bee9f4df4517b463a048146760ac62b484f
Samir Sinha <samir@cinekine.com>
2022-10-31 08:34:48 -0700

    A bit too zealous removing cross-platform code

--- host/platform/host_linux.c
+++ host/platform/host_linux.c
@@ -1,5 +1,8 @@
 #include "clem_host_platform.h"
 
+#include <X11/XKBlib.h>
+#include <X11/keysym.h>
+
 #include <assert.h>
 
 #define _GNU_SOURCE
@@ -7,6 +10,18 @@
 #include <sys/syscall.h>
 
 #include <uuid/uuid.h>
+
+
+//  seems to be a reliable way to call getcpu() regardless of glibc/distribution
+static inline unsigned local_getcpu() {
+    #ifdef SYS_getcpu
+    int cpu, status;
+    status = syscall(SYS_getcpu, &cpu, NULL, NULL);
+    return (status == -1) ? status : cpu;
+    #else
+    return UINT32_MAX; // unavailable
+    #endif
+}
 
 unsigned clem_host_get_processor_number() {
   return local_getcpu();
