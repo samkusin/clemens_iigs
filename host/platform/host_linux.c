33618e6648d6656d3df7f93a0b355564d240e570
Samir Sinha <samir@cinekine.com>
2022-11-27 07:23:09 -0800

    Stub joystick implemention for linux

--- host/platform/host_linux.c
+++ host/platform/host_linux.c
@@ -6,28 +6,31 @@
 #include <assert.h>
 
 #define _GNU_SOURCE
-#include <unistd.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 
 #include <uuid/uuid.h>
 
-
 //  seems to be a reliable way to call getcpu() regardless of glibc/distribution
 static inline unsigned local_getcpu() {
-    #ifdef SYS_getcpu
+#ifdef SYS_getcpu
     int cpu, status;
     status = syscall(SYS_getcpu, &cpu, NULL, NULL);
     return (status == -1) ? status : cpu;
-    #else
+#else
     return UINT32_MAX; // unavailable
-    #endif
+#endif
 }
 
-unsigned clem_host_get_processor_number() {
-  return local_getcpu();
+unsigned clem_host_get_processor_number() { return local_getcpu(); }
+
+void clem_host_uuid_gen(ClemensHostUUID *uuid) {
+    assert(sizeof(uuid_t) <= sizeof(uuid->data));
+    uuid_generate(uuid->data);
 }
 
-void clem_host_uuid_gen(ClemensHostUUID* uuid) {
-  assert(sizeof(uuid_t) <= sizeof(uuid->data));
-  uuid_generate(uuid->data);
-}
+void clem_joystick_open_devices(unsigned provider) {}
+
+unsigned clem_joystick_poll(ClemensHostJoystick *joysticks) { return 0; }
+
+void clem_joystick_close_devices() {}
