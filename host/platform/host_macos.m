4a7844d31aa39c827284e7bc6905a1d6e9260796
Samir Sinha <samir@cinekine.com>
2023-02-11 22:21:19 -0800

    Basic GameController support (#49)

--- host/platform/host_macos.m
+++ host/platform/host_macos.m
@@ -55,32 +55,156 @@ void clem_host_platform_init() {}
 
 void clem_host_platform_terminate() {}
 
-static NSArray<GCController*> *controllers_ = nil;
+@interface ClemensGameControllerDispatch: NSObject {
+    NSPointerArray* controllers; 
+}
 
-static void _open_gamecontrollers() {
-    //NSNotifcationCenter* notificationCenter = [NSNotificationCenter defaultCenter];
+@property (readonly) unsigned controllerCount;
 
-    controllers_ = [GCController controllers];
-    unsigned controllerIndex;
-    printf("%u count\n", controllers_.count);
-    for (controllerIndex = 0; controllerIndex < controllers_.count; ++controllerIndex) {
-        GCPhysicalInputProfile *profile = controllers_[controllerIndex].physicalInputProfile;
-        if (profile != nil) {
-            NSLog(@"Elements = %u", (unsigned)profile.allElements.count);
+- (BOOL)queryInputsAtIndex: (NSUInteger)atIndex withJoystick:(ClemensHostJoystick *)inputs;
+
+@end
+
+@implementation ClemensGameControllerDispatch
+
+- (unsigned)controllerCount {
+    return controllers.count;
+}
+
+- (instancetype)init {
+    self = [super init];
+    controllers = [[NSPointerArray alloc] init];
+    return self;
+}
+
+-(void)onControllerConnected:(NSNotification *) note {
+    GCController * controller = note.object;
+    unsigned index;
+    for (index = 0; index < controllers.count; ++index) {
+        if ([controllers pointerAtIndex: index] == nil) {
+            [controllers replacePointerAtIndex: index withPointer: controller];
+            break;
+        }
+    }
+    if (index >= controllers.count) {
+        [controllers addPointer: controller];
+    }
+    GCExtendedGamepad* gamepad = [controller extendedGamepad];
+    GCMicroGamepad* micro = [controller microGamepad];
+    if (gamepad != nil && micro != nil) {
+        NSLog(@"[%u] ATTACHED: %s and has extendedGamepad=%s, microGamepad=%s", 
+              index, controller.vendorName.UTF8String, 
+              gamepad != nil ? "YES" : "NO", micro != nil ? "YES" : "NO");
+    } else {
+        NSLog(@"[%u] ATTACHED: %s with unknown profile", index, controller.vendorName.UTF8String );
+    }
+}
+
+-(void)onControllerDisconnected:(NSNotification *) note {
+    GCController * controller = note.object;
+    unsigned index;
+    for (index = 0; index < controllers.count; ++index) {
+        if ([controllers pointerAtIndex: index] == controller) {
+            NSLog(@"[%u] DISCONNECTED: %s", index, controller.vendorName.UTF8String );    
+            [controllers replacePointerAtIndex: index withPointer: nil];
+            break;
         }
     }
 }
 
+- (BOOL)queryInputsAtIndex: (NSUInteger)index withJoystick:(ClemensHostJoystick *)inputs {
+    if (index >= controllers.count) return NO;
+    GCController* controller = [controllers pointerAtIndex: index];
+    inputs->isConnected = false;
+
+    if (controller == nil) {
+        return NO;
+    }
+    //  Apple Joysticks are at most two axis and two button - but check for all supported
+    //  apple gamepad types by complexity order (extended first, then fallback to microGamepad) 
+    //  gamepad profile.
+    GCExtendedGamepad* gamepad = [controller extendedGamepad];
+    GCMicroGamepad* micro = [controller microGamepad];
+    if (gamepad == nil && micro == nil) return NO;
+
+    inputs->isConnected = true;
+    inputs->buttons = 0;
+    if (gamepad) {
+        inputs->x[0] = gamepad.leftThumbstick.xAxis.value * CLEM_HOST_JOYSTICK_AXIS_DELTA;
+        inputs->x[1] = gamepad.rightThumbstick.xAxis.value * CLEM_HOST_JOYSTICK_AXIS_DELTA;
+        inputs->y[0] = -gamepad.leftThumbstick.yAxis.value * CLEM_HOST_JOYSTICK_AXIS_DELTA;
+        inputs->y[1] = -gamepad.rightThumbstick.yAxis.value * CLEM_HOST_JOYSTICK_AXIS_DELTA;;
+        if (gamepad.buttonA.pressed) {
+            inputs->buttons |= 0x1;
+        }
+        if (gamepad.buttonX.pressed) {
+            inputs->buttons |= 0x2;
+        }
+        if (gamepad.buttonB.pressed) {
+            inputs->buttons |= 0x4;
+        }
+        if (gamepad.buttonY.pressed) {
+            inputs->buttons |= 0x8;
+        }
+    } else if (micro) {
+        inputs->x[0] = micro.dpad.xAxis.value * CLEM_HOST_JOYSTICK_AXIS_DELTA;
+        inputs->x[1] = 0;
+        inputs->y[0] = -micro.dpad.yAxis.value * CLEM_HOST_JOYSTICK_AXIS_DELTA;
+        inputs->y[1] = 0;
+        if (micro.buttonA.pressed) {
+            inputs->buttons |= 0x1;
+        }
+        if (micro.buttonX.pressed) {
+            inputs->buttons |= 0x2;
+        }
+    }
+    return YES;
+}
+@end
+
+static ClemensGameControllerDispatch* controllerDispatch_ = nil;
+
+static void _open_gamecontrollers() {
+    controllerDispatch_ = [[ClemensGameControllerDispatch alloc] init];
+    NSNotificationCenter* notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter addObserver: controllerDispatch_
+                           selector: @selector(onControllerConnected:)
+                               name: GCControllerDidConnectNotification
+                             object: nil];
+    [notificationCenter addObserver: controllerDispatch_
+                           selector: @selector(onControllerDisconnected:)
+                               name: GCControllerDidDisconnectNotification
+                             object: nil];
+}
+
 void clem_joystick_open_devices(const char *provider) {
+    clem_joystick_close_devices();
     if (strncmp(provider, CLEM_HOST_JOYSTICK_PROVIDER_GAMECONTROLLER, 32) == 0) {
         _open_gamecontrollers();
     }
 }
 
 unsigned clem_joystick_poll(ClemensHostJoystick *joysticks) {
-    return 0;
+    unsigned index = 0;
+    if (controllerDispatch_) {
+        for (; index < controllerDispatch_.controllerCount && index < CLEM_HOST_JOYSTICK_LIMIT; ++index) {
+            if ([controllerDispatch_ queryInputsAtIndex: index withJoystick: &joysticks[index]] == NO) {
+                joysticks[index].isConnected = false;
+            }
+        }
+    }
+    return index;
 }
 
 void clem_joystick_close_devices() {
-    controllers_ = nil;
+    if (controllerDispatch_ != nil) {
+        NSNotificationCenter* notificationCenter = [NSNotificationCenter defaultCenter];
+        [notificationCenter removeObserver: controllerDispatch_
+                                      name: GCControllerDidDisconnectNotification
+                                    object: nil];
+        [notificationCenter removeObserver: controllerDispatch_
+                                      name: GCControllerDidConnectNotification
+                                    object: nil];
+        controllerDispatch_ = nil;
+    }
 }
