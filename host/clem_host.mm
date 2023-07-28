7e9a7a3d533aa6bdc2fe6ab20e6057e47b52d843
Samir Sinha <samir@cinekine.com>
2023-07-28 08:05:05 -0700

    Fix macOS build issue

--- host/clem_host.mm
+++ host/clem_host.mm
@@ -10,6 +10,7 @@
 #include "sokol/sokol_app.h"
 #include "sokol/sokol_audio.h"
 #include "sokol/sokol_gfx.h"
+#include "sokol/sokol_gfx_ext.h"
 #include "sokol/sokol_glue.h"
 #include "sokol/sokol_imgui.h"
 #include "sokol/sokol_time.h"
